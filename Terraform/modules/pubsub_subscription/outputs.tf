d47291316aa3eeaa86e87d3e1194808f76c0a82a
Adham Ayad <adhamayad@localhost.localdomain>
2025-11-07 21:46:23 +0200

    Fix Terraform Error

--- Terraform/modules/pubsub_subscription/outputs.tf
+++ Terraform/modules/pubsub_subscription/outputs.tf
@@ -1,9 +1,9 @@
 output "name" {
   description = "The name of the created subscription."
-  value       = google_pubsub_subscription.this.name
+  value       = google_pubsub_subscription.subscription.name
 }
 
 output "id" {
   description = "The full ID of the created subscription."
-  value       = google_pubsub_subscription.this.id
+  value       = google_pubsub_subscription.subscription.id
 }
