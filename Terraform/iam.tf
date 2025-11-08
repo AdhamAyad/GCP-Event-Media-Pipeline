a8e1105812d0548f6c55873de24407632eee5cb4
Adham Ayad <adhamayad@localhost.localdomain>
2025-11-08 20:41:34 +0200

    Update IAM Premission

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -21,10 +21,16 @@ module "backend_sa" {
     ]
 }
 
-resource "google_storage_bucket_iam_member" "public_access" {
+resource "google_storage_bucket_iam_member" "backend_processed_bucket_reader" {
   bucket = google_storage_bucket.gcp_event_media_processed_bucket.name
-  role   = "roles/storage.objectViewer"
-  member = "allUsers"  
+  role   = "roles/storage.objectViewer"   
+  member = "serviceAccount:${module.backend_sa.service_account_email}"
+}
+
+resource "google_storage_bucket_iam_member" "backend_processed_bucket_bucket_reader" {
+  bucket = google_storage_bucket.gcp_event_media_processed_bucket.name
+  role   = "roles/storage.legacyBucketReader"   
+  member = "serviceAccount:${module.backend_sa.service_account_email}"
 }
 
 resource "google_storage_bucket_iam_member" "backend_raw_bucket_reader" {
