836f9bd023e1e90d01ef51e47730f312896d8698
Adham Ayad <adhamayad@localhost.localdomain>
2025-11-08 19:23:44 +0200

    Update Service Account Module

--- Terraform/modules/service_account/outputs.tf
+++ Terraform/modules/service_account/outputs.tf
@@ -1,3 +1,8 @@
 output "service_account_email" {
   value = google_service_account.service_account_module.email
 }
+
+output "name" {
+  description = "The fully qualified name (ID) of the service account."
+  value       = google_service_account.service_account_module.name
+}
