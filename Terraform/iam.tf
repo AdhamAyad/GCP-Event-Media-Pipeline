050821f2a9bac0bb797052021361f37183ffd326
Adham Ayad <adhamayad@localhost.localdomain>
2025-11-08 19:20:17 +0200

    Update Backend Premission

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -21,6 +21,12 @@ module "backend_sa" {
     ]
 }
 
+resource "google_service_account_iam_member" "backend_sa_token_creator" {
+  service_account_id = module.backend_sa.name 
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "serviceAccount:${module.backend_sa.service_account_email}"
+}
+
 resource "google_storage_bucket_iam_member" "backend_processed_bucket_reader" {
   bucket = google_storage_bucket.gcp_event_media_processed_bucket.name
   role   = "roles/storage.legacyBucketReader"
