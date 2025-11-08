d941aea12cc3780e6b0bced1de0413dc60c1de80
Adham Ayad <adhamayad@localhost.localdomain>
2025-11-08 21:30:29 +0200

    Add Alert

--- Terraform/alert.tf
+++ Terraform/alert.tf
@@ -11,39 +11,38 @@ resource "google_monitoring_notification_channel" "email_channel" {
 resource "google_monitoring_alert_policy" "dlt_alert" {
   project      = var.project_id
   display_name = "Critical Pipeline Failure (DLT)"
-  combiner     = "OR" 
-  
+  combiner     = "OR"
+
   conditions {
     display_name = "Message received in Dead Letter Topic"
-    
+
     condition_threshold {
-      filter = join(" AND ", [
-        "resource.type = \"pubsub_topic\"",
-        "metric.type = \"pubsub.googleapis.com/topic/send_request_count\"",
-        "resource.label.\"topic_id\" = \"${google_pubsub_topic.dlt_topic.name}\""
-      ])
+      filter = "resource.type = \"pubsub_topic\" AND metric.type = \"pubsub.googleapis.com/topic/send_request_count\" AND resource.label.\"topic_id\" = \"${google_pubsub_topic.dlt_topic.name}\""
       
-      duration = "60s" 
-      
-      comparison = "COMPARISON_GT"
-      threshold_value = 0 
-      
-      
+      comparison      = "COMPARISON_GT"
+      threshold_value = 0
+      duration        = "60s"
+
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_SUM"
+      }
+
       trigger {
-        count = 1 
+        count = 1
       }
     }
   }
-  
+
   notification_channels = [
     google_monitoring_notification_channel.email_channel.id
   ]
-  
+
   documentation {
-    content = "A message failed all retries (5 times) in one of the workshops (Thumbnail, Display, Metadata, or AI) and was sent to the Dead Letter Topic. The pipeline is blocked. Check the DLT Topic and the Cloud Run logs immediately."
+    content   = "A message failed all retries and was sent to the DLT. Check Cloud Run / Pub/Sub logs immediately."
     mime_type = "text/markdown"
   }
-  
+
   depends_on = [
     google_monitoring_notification_channel.email_channel,
     google_pubsub_topic.dlt_topic
